9ca1d0f7e6282d9444f6fef84e0ac000d1dafc62
Abdullah Numan <anewman15@hotmail.com>
2025-04-02 12:33:32 +0600

    feat: add dashboard/customers/create page

---
+++ app/dashboard/customers/create/page.tsx
@@ -0,0 +1,23 @@
+import CreateCustomerForm from '@/app/ui/customers/create-customer-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchCustomers } from '@/app/lib/data';
+
+export default async function Page() {
+  const customers = await fetchCustomers();
+
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Customers', href: '/dashboard/customers' },
+          {
+            label: 'Create',
+            href: '/dashboard/customers/create',
+            active: true,
+          },
+        ]}
+      />
+      <CreateCustomerForm />
+    </main>
+  );
+};
