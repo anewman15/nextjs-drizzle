b7fbf74476715d3d710f8315a3517a6b1a88d300
Abdullah Numan <anewman15@hotmail.com>
2025-04-02 12:11:45 +0600

    feat: update invoice create Form

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -1,5 +1,10 @@
-import { CustomerField } from '@/app/lib/definitions';
+"use client"
+
+import { ReactNode } from 'react';
 import Link from 'next/link';
+import { redirect } from 'next/navigation';
+import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import {
   CheckIcon,
   ClockIcon,
@@ -7,10 +12,34 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
+import { CustomerField, NewInvoice, SchemaNewInvoice } from '@/app/lib/zod-types';
+
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
+
+  const defaultValues: NewInvoice = {
+    customer_id: "",
+    date: new Date().toISOString().split("T")?.[0],
+    amount: 0.00,
+    status: "pending",
+  };
+
+  const { formState: { errors }, register, handleSubmit } = useForm({
+    resolver: zodResolver(SchemaNewInvoice),
+    defaultValues,
+
+    mode: "onChange",
+    criteriaMode: "all",
+    shouldFocusError: true,
+    reValidateMode: "onChange"
+  });
+
+  const createNewInvoice: SubmitHandler<NewInvoice> = async (data) => {
+    redirect("/dashboard/invoices");
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(createNewInvoice)}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -20,9 +49,9 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
           <div className="relative">
             <select
               id="customer"
-              name="customerId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue=""
+              {...register("customer_id")}
             >
               <option value="" disabled>
                 Select a customer
@@ -34,6 +63,11 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               ))}
             </select>
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+            {
+              errors?.customer_id && (
+                <span className="text-red-500 text-xs">{errors.customer_id.message as ReactNode}</span>
+              )
+            }
           </div>
         </div>
 
@@ -46,13 +80,18 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
             <div className="relative">
               <input
                 id="amount"
-                name="amount"
+                {...register("amount", { valueAsNumber: true })}
                 type="number"
                 step="0.01"
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+              {
+                errors?.amount && (
+                  <span className="text-red-500 text-xs">{errors.amount.message as ReactNode}</span>
+                )
+              }
             </div>
           </div>
         </div>
@@ -67,7 +106,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               <div className="flex items-center">
                 <input
                   id="pending"
-                  name="status"
+                  {...register("status")}
                   type="radio"
                   value="pending"
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
@@ -82,7 +121,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               <div className="flex items-center">
                 <input
                   id="paid"
-                  name="status"
+                  {...register("status")}
                   type="radio"
                   value="paid"
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
@@ -95,6 +134,11 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 </label>
               </div>
             </div>
+            {
+              errors?.status && (
+                <span className="text-red-500 text-xs">{errors.status.message as ReactNode}</span>
+              )
+            }
           </div>
         </fieldset>
       </div>
@@ -109,4 +153,4 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
       </div>
     </form>
   );
-}
+};
