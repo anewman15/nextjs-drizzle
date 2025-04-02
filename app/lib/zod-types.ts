6f682b87a8e566367bba4e43b6635714f1491e4e
Abdullah Numan <anewman15@hotmail.com>
2025-04-02 10:50:38 +0600

    chore: add zod types for server actions

---
+++ app/lib/zod-types.ts
@@ -0,0 +1,37 @@
+
+import * as zod from "zod";
+
+export const SchemaCustomer = zod.object({
+  id: zod.string().uuid(),
+  name: zod.string()
+    .min(1, { message: "Name cannot be empty" })
+    .max(55, { message: "Name should not exceed 55 characters" }),
+  email: zod.string().email({ message: "Enter a valid email" }),
+  image_url: zod.string(),
+});
+
+export const SchemaNewCustomer = SchemaCustomer.omit({
+  id: true,
+  image_url: true,
+});
+
+export const SchemaInvoice = zod.object({
+  id: zod.string().uuid(),
+  date: zod.string().date(),
+  amount: zod
+    .number()
+    .positive({ message: "Invoice amount must be positive" })
+    .min(1, { message: "Minimum amount must be 1$" }),
+  status: zod.enum(["paid", "pending"]).default("pending"),
+  customer_id: zod.string().uuid({ message: "Please choose a customer " }),
+});
+
+export const SchemaNewInvoice = SchemaInvoice.omit({
+  id: true,
+});
+
+export type Customer = zod.infer<typeof SchemaCustomer>;
+export type NewCustomer = zod.infer<typeof SchemaNewCustomer>;
+export type CustomerField = Pick<Customer, "id" | "name">;
+export type Invoice = zod.infer<typeof SchemaInvoice>;
+export type NewInvoice = zod.infer<typeof SchemaNewInvoice>;
