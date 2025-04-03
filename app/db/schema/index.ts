2375d115962f26a709cc34b26b5c09e6dc0230c8
Abdullah Numan <anewman15@hotmail.com>
2025-04-04 02:58:20 +0600

    feat: export schema defs

---
+++ app/db/schema/index.ts
@@ -0,0 +1,4 @@
+export { customers } from "@/app/db/schema/customers.schema";
+export { invoices } from "@/app/db/schema/invoices.schema";
+export { revenues } from "@/app/db/schema/revenues.schema";
+export { customersRelations, invoicesRelations } from "./relations.schema";
