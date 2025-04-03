ca4b7dabbfe608028593ba3cbb7d450032d9aad8
Abdullah Numan <anewman15@hotmail.com>
2025-04-04 02:57:21 +0600

    feat: add drizzle schema defs for customers

---
+++ app/db/schema/customers.schema.ts
@@ -0,0 +1,32 @@
+import { pgTable, uuid, varchar } from "drizzle-orm/pg-core";
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+import * as zod from "zod";
+
+export const customers = pgTable("customers", {
+  id: uuid("id").primaryKey().unique().defaultRandom(),
+  name: varchar("name").notNull(),
+  email: varchar("email").unique().notNull(),
+  image_url: varchar("image_url")
+    .notNull()
+    .default("/customers/balazs-orban.png"),
+});
+
+export const SchemaCustomer = createSelectSchema(customers);
+export const SchemaCustomerList = zod.array(SchemaCustomer);
+export const SchemaNewCustomer = createInsertSchema(customers, {
+  name: (schema) =>
+    schema
+      .min(1, { message: "Name cannot be empty" })
+      .max(55, { message: "Name should not exceed 55 characters" }),
+  email: (schema) =>
+    schema
+      .email({ message: "Enter a valid email" })
+      .nonempty("Please enter your email"),
+  image_url: (schema) => schema.optional(),
+}).omit({
+  id: true,
+});
+
+export type Customer = zod.infer<typeof SchemaCustomer>;
+export type NewCustomer = zod.infer<typeof SchemaNewCustomer>;
+export type CustomerField = Pick<Customer, "id" | "name">;
