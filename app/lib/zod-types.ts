71f7f358e3c7d0f0570fd33458bad1a6b1e6c5fd
Abdullah Numan <anewman15@hotmail.com>
2025-04-04 15:25:10 +0600

    fix: update zod-types

--- app/lib/zod-types.ts
+++ app/lib/zod-types.ts
@@ -1,28 +1,29 @@
-
 import * as zod from "zod";
 
 export const SchemaCustomer = zod.object({
   id: zod.string().uuid(),
-  name: zod.string()
+  name: zod
+    .string()
     .min(1, { message: "Name cannot be empty" })
     .max(55, { message: "Name should not exceed 55 characters" }),
   email: zod.string().email({ message: "Enter a valid email" }),
   image_url: zod.string(),
 });
 
-export const SchemaNewCustomer = SchemaCustomer.omit({
-  id: true,
-  image_url: true,
-});
+export const SchemaNewCustomer = SchemaCustomer
+  .omit({
+    id: true,
+  })
+  .partial({ image_url: true });
 
 export const SchemaInvoice = zod.object({
-  id: zod.string().uuid(),
+  id: zod.string().uuid().nonempty(),
   date: zod.string().date(),
   amount: zod
     .number()
     .positive({ message: "Invoice amount must be positive" })
     .min(1, { message: "Minimum amount must be 1$" }),
-  status: zod.enum(["paid", "pending"]).default("pending"),
+  status: zod.enum(["paid", "pending"]),
   customer_id: zod.string().uuid({ message: "Please choose a customer " }),
 });
 
